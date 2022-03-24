0a08821e60b72725ca6c8f6dae152cf4acac7e61
Joao Gustavo <gustavojoao28@gmail.com>
2022-03-24 10:03:03 -0300

    HomeView

--- src/main/java/br/com/econgelados/application/ApplicationMain.java
+++ src/main/java/br/com/econgelados/application/ApplicationMain.java
@@ -1,23 +1,37 @@
 package br.com.econgelados.application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-public class ApplicationMain<GenericApplicationContext> extends Application{
+public class ApplicationMain extends Application{
 	
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/com/econgelados/gui/HomeView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Econgelados");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
--- src/main/java/br/com/econgelados/application/module-info.java
+++
@@ -1,6 +0,0 @@
-module econgelados {
-	requires javafx.controls;
-	requires spring.boot.autoconfigure;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
